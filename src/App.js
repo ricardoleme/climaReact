c9b5f6fff82b6a462768211d4971f7717b2ce543
Ricardo Leme <ricardo.leme@gmail.com>
2021-03-05 07:14:22 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { MdGpsFixed } from "react-icons/md";
 
 function App() {
+
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+  function success(pos) {
+    var crd = pos.coords;
+  
+    console.log('Sua posição atual é:');
+    console.log('Latitude : ' + crd.latitude);
+    console.log('Longitude: ' + crd.longitude);
+    console.log('Mais ou menos ' + crd.accuracy + ' metros.');
+  };
+  
+  function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  };
+
+  function obterGeoLocalizacao() {
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">FateClima</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Início</Nav.Link>
+          <Nav.Link href="#contato">Contato</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Informe a cidade..." className="ml-sm-4" />
+          <Button variant="success" size="sm"
+            title="Obter a localização"
+            onClick={obterGeoLocalizacao}>
+            <MdGpsFixed color="white" /> Obter a Localização
+          </Button>
+          <Button variant="outline-light">Procurar</Button>
+        </Form>
+      </Navbar>
+
+    </>
   );
 }
 
