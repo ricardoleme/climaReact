c51135559f0042f0edb12f8a5192e5ca2f9443bd
Ricardo Leme <ricardo.leme@gmail.com>
2021-03-08 17:04:49 -0300

    Adicionado a chamada da API

--- src/App.js
+++ src/App.js
@@ -7,26 +7,32 @@ import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Card from 'react-bootstrap/Card'
-import CardDeck from 'react-bootstrap/CardDeck'
+import Row from 'react-bootstrap/Row'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { FaCloudversify } from "react-icons/fa"
 import { FaSpinner } from "react-icons/fa"
 import { FaCloudRain } from "react-icons/fa"
+import { FaArrowDown, FaArrowUp } from "react-icons/fa"
+
 
 
 /* API Geo Referenciamento
 * https://opencagedata.com/users/sign_up
 * API Fotos
 * https://unsplash.com/oauth/applications
-* ZzdMKScR_-MgaspHZbga5ujrJWc6LRAaEbcBfCuA_a4
+* 
 * https://api.unsplash.com/search/photos?query=Itu&client_id=71fab1070168597fcfd2bf922067b1b266a00074285460c4fa4e1967dff36384&page=1&w=1500&dpi=2
-
+*
+* API Weather
+https://openweathermap.org/api
+http://api.openweathermap.org/data/2.5/weather?q=Indaiatuba,br&lang=pt&units=metric&appid=62f17a7cff53cf644e40267fda2b5aa3
 */
 
 function App() {
   const [erro, setErro] = useState(null)
   const [cidade, setCidade] = useState('')
+  const [clima, setClima] = useState(null)
 
   //Fonte: https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPositionError/code
   const errosGeo = [{ "codigo": 1, "texto": "Não foi dada permissão para o sistema poder encontrar a sua localização" },
@@ -38,7 +44,7 @@ function App() {
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition(function (position) {
         //console.log(position)
-        obtemCidade(position.coords.latitude, position.coords.longitude)
+        position.coords.latitude && obtemCidade(position.coords.latitude, position.coords.longitude)
       }, function (error) {
         //console.log(error)
         setErro(error.code)
@@ -56,26 +62,28 @@ function App() {
           console.error('Houve um problema ao efetuar a requisição: ' + error.message);
         });
     }
+
+   
   }, [])
 
-  async function obtemFotos(){
-    let url=`https://api.unsplash.com/search/photos?query=${cidade}&client_id=71fab1070168597fcfd2bf922067b1b266a00074285460c4fa4e1967dff36384&page=1&w=1500&dpi=2
-    `
-    await fetch(url)
+
+  async function obtemClima(cidade){
+    const apiweather = process.env.REACT_APP_OPENWEATHER
+    let urlClima=`http://api.openweathermap.org/data/2.5/weather?q=${cidade}&lang=pt&units=metric&appid=${apiweather}`
+    await fetch(urlClima)
       .then(response => response.json())
       .then(data => {
-        console.log(data.results)
-       
+       setClima(data)
       })
       .catch(function (error) {
         console.error('Houve um problema ao efetuar a requisição: ' + error.message);
       });
   }
-obtemFotos()
+
 
   return (
     <>
-      <Navbar bg="primary" variant="dark">
+      <Navbar bg="primary">
         <Navbar.Brand href="#home">FateClima</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="#home">Início</Nav.Link>
@@ -88,7 +96,7 @@ obtemFotos()
           { !cidade && !erro &&
           <FaSpinner className="spinner" color="white" title="Obtendo a cidade..."/>            
            }
-          <Button variant="outline-dark" ><FaCloudversify color="white" /> Obter Clima</Button>
+          <Button variant="outline-dark" onClick={() => {obtemClima(cidade)}} ><FaCloudversify color="white" /> Obter Clima</Button>
         </Form>
       </Navbar>
       
@@ -105,21 +113,32 @@ obtemFotos()
   <h1><FaCloudRain/> FateClima</h1>
   <p>
     Consulte o clima de qualquer cidade do mundo. <br></br>
-    App desenvolvido em ReactJS e integrado com as APIs Opencagedata, Unsplash
+    App desenvolvido em ReactJS e integrado com as APIs Opencagedata e OpenWeatherMap
   </p>
 </Jumbotron >
-      <CardDeck>
-      <Card>
-    <Card.Img variant="top" src="http://holder.js/100px160" />
+{clima &&
+
+<Row className="justify-content-center">
+
+      <Card bg="primary" className="cartao text-center">
+      <Card.Header>
+        <h2>{clima.name}</h2>
+        <h3>{clima.main.temp}&#x2103;</h3> {/*https://www.toptal.com/designers/htmlarrows/symbols/*/}
+        <h5>min: {clima.main.temp_min}&#x2103;<FaArrowDown className="text-danger" /> - máx: {clima.main.temp_max}&#x2103;<FaArrowUp className="text-success"/></h5>
+      <Card.Img  src={`http://openweathermap.org/img/wn/${clima.weather[0].icon}@4x.png`}  title={clima.weather[0].description}/>
+    <h3 className="text-white">{clima.weather[0].description}</h3>
+    </Card.Header>
     <Card.Body>
-      <Card.Title>Card title that wraps to a new line</Card.Title>
+
+      <Card.Title>Previsão do Tempo</Card.Title>
       <Card.Text>
-        This is a longer card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
+
       </Card.Text>
     </Card.Body>
+    <Card.Footer className="text-white">Atualizado em: {new Date(clima.dt * 1000).toLocaleString('pt-BR',{timeZone: 'America/Sao_Paulo'})}</Card.Footer>
   </Card>
-  </CardDeck>
+  </Row>
+}
     </>
   );
 }
